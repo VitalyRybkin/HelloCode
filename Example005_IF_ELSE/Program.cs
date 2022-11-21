c372992da98ebdb2364d4df9c031d9fe311e96a1
Vitaly <vitaly.rybkin@gmail.com>
2022-11-21 10:57:41 +0300

    add if_else

---
+++ Example005_IF_ELSE/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter a username: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha") {
+    Console.WriteLine("Wow! Hello, pretty Masha!");
+}
+else {
+    Console.WriteLine("Hello, " + username + "!");
+}
